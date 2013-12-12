d9178e7a1a0c35b2f7cbffd618fdc0eb08117fec
Lyndsey <lyndsey@lyndseyjane.com>
2013-12-12 15:35:17 -0500

    Draws 10000 live updates to the canvas.
    Added index.html and main.css file.

--- src/visual_rc/core.cljs
+++ src/visual_rc/core.cljs
@@ -12,14 +12,14 @@
 
 (def input-channel (chan 10))
 
-(defn make-socket [name address]
+(defn make-socket [lang address]
   (let [socket (js/WebSocket. address)]
     (doall
     (map #(aset socket (first %)(second %))
          [["onopen" (fn [] (log "open!"))]
           ["onclose" (fn [] (log "closed!"))]
           ["onerror" (fn [e] (log (str "oh no!:" e)))]
-          ["onmessage" (fn [m] (go (>! input-channel [name m])))]]))
+          ["onmessage" (fn [m] (go (>! input-channel [lang m])))]]))
     socket))
 
 (def websockets (into {} (for [[lang address] config]
@@ -38,9 +38,33 @@
     (set! (.-fillStyle context) rgbstr)
     (.fillRect context 0 0 w h)))
 
+(defn draw-circle [context lang]
+  (let [color (case lang
+             :english "#314170" ;dark blue
+             :german  "#ED8931" ;orange
+             :russian "#99B3FB" ;light blue
+             :japanese "#DF4527" ;red
+             :french "#FFFFFF" ;white
+             :italian "#53C549" ;green
+             :polish "#9972A8" ;purple
+             :spanish "#E8CD3A" ;yellow
+             "000000") ;oops
+        radius 10
+        x (+ (rand-int (- (.-width canvas) (* 2 radius))) radius)
+        y (+ (rand-int (- (.-height canvas) (* 2 radius))) radius)
+        ]
+    (set! (.-fillStyle context) color)
+    (.beginPath context)
+    (.arc context x y radius 0 (* 2 (.-PI js/Math) 10) false)
+    (.fill context)))
+
+(set! (.-fillStyle context) "#D9D9D9")
+(.fillRect context 0 0 (.-width canvas) (.-height canvas))
+
 (go
- (loop [x 100]
+ (loop [x 10000]
    (when (pos? x)
-     (log (<! input-channel))
-     (set-random-bg context)
+     (let [color (first (<! input-channel))]
+       (log color)
+           (draw-circle context color))
      (recur (dec x)))))
